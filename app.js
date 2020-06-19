3fae6db6ff77ce415efb626e841e4f1c4d043c59
Abhilasha <abhilasha.y.1305@gmail.com>
2020-06-19 11:53:00 +0530

    Add gitignore Procfile and update parts

--- app.js
+++ app.js
@@ -149,9 +149,12 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-
-app.listen(3000,function(req, res){
-  console.log("The server is runnng on port 3000...");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port,function(req, res){
+  console.log("The server is runnng on port ..."+ port);
 });
 
 
